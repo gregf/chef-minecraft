88074e339f70f46dea92b1f5b64e64f5ebd30e5a
Greg Fitzgerald <greg@gregf.org>
2017-09-09 17:20:05 -0400

    Prepare for release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  'greg@gregf.org'
 license           'Apache-2.0'
 description       'Installs/Configures minecraft server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.5.2'
+version           '0.6.0'
 name              'minecraft'
 issues_url         'https://github.com/gregf/chef-minecraft/issues'
 source_url        'https://github.com/gregf/chef-minecraft/'
